c62cf2cb605b0d79d319f1894ed95498136b217a
ddiablou <jekem13936@jo6s.com>
2023-04-01 20:40:15 +0200

    fix(): unused deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,17 @@
 
 import { useEffect, useState } from "react";
 import { AddSquare, SearchNormal1, Star1 } from "iconsax-react";
-import { SEARCH_API_URL, SHORTCUTS } from "@/lib/constants";
-import { useRouter } from "next/router";
+import { SEARCH_API_URL } from "@/lib/constants";
 import Button from "@/components/button";
 
 export default function Home() {
   const [time, setTime] = useState("00:00");
-  const [query, setQuery] = useState<string | null>()
+  const [query, setQuery] = useState("trydevhub+github");
 
   const date = new Date();
   const hour = date.getHours();
   const minute = date.getMinutes();
   const format = hour >= 12 ? "PM" : "AM";
-
-  const shortcuts = SHORTCUTS.map((shortcut, index) => (
-    <div
-      key={index}
-      className="w-auto bg-rosePine-surface p-4 truncate hover:bg-rosePine-overlay cursor-pointer"
-    >
-      {shortcut.name}
-    </div>
-  ));
 
   useEffect(() => {
     setTime(`${hour}:${minute < 10 ? `0${minute}` : minute} ${format}`);
@@ -33,16 +23,12 @@ export default function Home() {
       <h1 className="text-5xl font-bold text-rosePine-text">{time}</h1>
       <div className="px-5 py-3 max-w-xl bg-rosePine-surface hover:bg-rosePine-overlay rounded-full w-full flex">
         <input
-          onChange={(e) =>
-            setQuery(e.target.value)
-          }
+          onChange={(e) => setQuery(e.target.value.replaceAll(" ", "+"))}
           placeholder="Search on the web..."
           className="outline-none placeholder:text-rosePine-muted placeholder:italic text-rosePine-subtle bg-transparent w-full"
         />
         <SearchNormal1
-        onClick={() =>
-          (location.href = `${SEARCH_API_URL}?q=${query}`)
-        }
+          onClick={() => (location.href = `${SEARCH_API_URL}?q=${query}`)}
           variant="Bulk"
           size={25}
           className="text-rosePine-iris cursor-pointer"
